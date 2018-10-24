53092516ae7e4d883e0b9eea8cd774ec07b34037
Blake <rblakehampton@gmail.com>
2018-10-23 18:20:09 -0600

    Filter::info (row col flip)

--- Filter.cpp
+++ Filter.cpp
@@ -36,8 +36,8 @@ int Filter::getSize()
 void Filter::info()
 {
   cout << "Filter is.." << endl;
-  for (int col = 0; col < dim; col++) {
-    for (int row = 0; row < dim; row++) {
+  for (int row = 0; row < dim; row++) {
+    for (int col = 0; col < dim; col++) {
       int v = get(row, col);
       cout << v << " ";
     }
